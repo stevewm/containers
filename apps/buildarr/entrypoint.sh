859298ecfbc87655fc14911683bfc2c27937bc0d
stevewm <34654219+stevewm@users.noreply.github.com>
2024-08-08 11:53:03 -0700

    fix: get rid of envsubst

--- apps/buildarr/entrypoint.sh
+++ apps/buildarr/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-
-if [[ -n $BUILDARR_CONFIG_PATH ]]; then
-    envsubst < "$BUILDARR_CONFIG_PATH" | sponge "$BUILDARR_CONFIG_PATH"
-fi
-
 #shellcheck disable=SC2086
 exec \
     /usr/local/bin/buildarr \
